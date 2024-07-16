20a471e239a3680fb74806cc3f35be64fb6edc77
ACACIANh <acacian@ppfriends.co.kr>
2024-07-16 13:31:16 +0900

    init

---
+++ src/main/kotlin/com/example/multiexample/MultiExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.multiexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MultiExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<MultiExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/multiexample/MultiExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.multiexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
